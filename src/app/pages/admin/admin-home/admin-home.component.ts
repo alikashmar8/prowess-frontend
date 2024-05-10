f001c3f3a6e596d7d14bf0bdab3793b5f91d662e
alikashmar8 <alikashmar8@gmail.com>
2024-05-10 23:55:18 +0300

    add admin home page

--- src/app/pages/admin/admin-home/admin-home.component.ts
+++ src/app/pages/admin/admin-home/admin-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/services/alert.service';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { CommonService } from 'src/app/services/common.service';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-admin-home',
@@ -6,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home.component.css'],
 })
 export class AdminHomeComponent implements OnInit {
-  constructor() {}
+  stats: {
+    totalCustomers: number;
+    invoicesCollectedToday: number;
+    totalActiveEmployees: number;
+    projectedMonthlyIncome: number;
+  } = {
+    totalCustomers: 0,
+    invoicesCollectedToday: 0,
+    totalActiveEmployees: 0,
+    projectedMonthlyIncome: 0,
+  };
+  currentUser: User;
+  isLoading: boolean = true;
 
-  ngOnInit(): void {}
+  constructor(
+    private commonService: CommonService,
+    private alertService: AlertService,
+    private authService: AuthService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.currentUser = this.authService.currentUser;
+      this.stats = await this.commonService.getStats();
+      this.isLoading = false;
+    } catch (e) {
+      console.log(e);
+      this.alertService.toastError('Error fetching dashboard stats');
+    }
+  }
 }
