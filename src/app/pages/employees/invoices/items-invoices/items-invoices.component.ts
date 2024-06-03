c29b7a0cbb798f6a8fe12d96fdb2b7ea307ce408
alikashmar8 <alikashmar8@gmail.com>
2024-06-03 23:02:18 +0300

    Refactor items-invoices component to include pagination and filtering

--- src/app/pages/employees/invoices/items-invoices/items-invoices.component.ts
+++ src/app/pages/employees/invoices/items-invoices/items-invoices.component.ts
@@ -8,59 +8,88 @@ import { User } from 'src/models/user.model';
 @Component({
   selector: 'app-items-invoices',
   templateUrl: './items-invoices.component.html',
-  styleUrls: ['./items-invoices.component.css']
+  styleUrls: ['./items-invoices.component.css'],
 })
 export class ItemsInvoicesComponent implements OnInit {
   isLoading: boolean = true;
   invoices: Invoice[] = [];
+  take: number = 10;
+  skip: number = 0;
+  totalRecords: number = 0;
+  currentPage: number = 1;
   currentUser: User;
+  totalPages: number;
+  sum = '';
 
   constructor(
     private invoicesService: InvoicesService,
     private loadingService: LoadingService,
     private authService: AuthService
-  ) {
-
-  }
+  ) {}
 
   async ngOnInit(): Promise<void> {
-    try{
+    try {
       this.loadingService.appLoading(true);
-    let res = await this.invoicesService.getItemsInvoices();
-    this.invoices = res.data
-    this.currentUser = this.authService.currentUser;
-    this.loadingService.appLoading(false);
-    }catch(err){
+      let res = await this.invoicesService.getItemsInvoices({
+        take: this.take,
+        skip: this.skip,
+      });
+      console.log(res);
+
+      this.invoices = res.data;
+      this.sum = res.sum;
+      this.totalRecords = res.count;
+      this.totalPages = res.count > 0 ? Math.ceil(res.count / this.take) : 1;
+      this.currentUser = this.authService.currentUser;
+      this.loadingService.appLoading(false);
+    } catch (err) {
       this.authService.handleHttpError(err);
     }
   }
 
-  async searchClicked(data?:{
+  async searchClicked(data?: {
     search?: string;
     selectedEmployee?: string;
     selectedPlan?: string;
     startDateFilter?: Date;
     endDateFilter?: Date;
+    isPaid?: string;
     selectedLevel5Address?: string;
     selectedLevel4Address?: string;
     selectedLevel3Address?: string;
     selectedLevel2Address?: string;
     selectedLevel1Address?: string;
+    take?: number;
+    skip?: number;
   }) {
     try {
       this.isLoading = this.loadingService.appLoading(true);
-      this.invoices = await this.invoicesService.getItemsInvoices({
+      const res = await this.invoicesService.getItemsInvoices({
         search: data.search,
         employee_id: data.selectedEmployee,
         plan_id: data.selectedPlan,
         startDate: data.startDateFilter,
         endDate: data.endDateFilter,
+        isPaid: data.isPaid,
+        level5Address: data.selectedLevel5Address,
+        level4Address: data.selectedLevel4Address,
+        level3Address: data.selectedLevel3Address,
+        level2Address: data.selectedLevel2Address,
+        level1Address: data.selectedLevel1Address,
+        take: data.take,
+        skip: data.skip,
       });
+      this.invoices = res.data;
+      this.sum = res.sum;
+      this.totalRecords = res.count;
+      this.totalPages = res.count > 0 ? Math.ceil(res.count / this.take) : 1;
+
       this.isLoading = this.loadingService.appLoading(false);
     } catch (err) {
       this.authService.handleHttpError(err);
       this.isLoading = this.loadingService.appLoading(false);
-    }}
+    }
+  }
 
   async exportPDF() {
     try {
@@ -114,5 +143,4 @@ export class ItemsInvoicesComponent implements OnInit {
       this.isLoading = this.loadingService.appLoading(false);
     }
   }
-
 }
