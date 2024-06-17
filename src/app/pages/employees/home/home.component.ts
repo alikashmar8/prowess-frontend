530011b995a580d24f09e1733e386a7b2f753df0
alikashmar8 <alikashmar8@gmail.com>
2024-06-17 21:08:41 +0300

    Add received amounts listing to home component

--- src/app/pages/employees/home/home.component.ts
+++ src/app/pages/employees/home/home.component.ts
@@ -1,8 +1,12 @@
+import { UserRoles } from 'src/enums/user-roles.enum';
+import { ListReceivedAmountsComponent } from './../../../common/modals/list-received-amounts/list-received-amounts.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component, OnInit } from '@angular/core';
 import { AlertService } from 'src/app/services/alert.service';
 import { AuthService } from 'src/app/services/auth-service.service';
 import { CommonService } from 'src/app/services/common.service';
 import { User } from 'src/models/user.model';
+import { AddReceivedAmountComponent } from 'src/app/common/modals/add-received-amount/add-received-amount.component';
 
 @Component({
   selector: 'app-home',
@@ -15,20 +19,26 @@ export class EmployeesHomeComponent implements OnInit {
     customers_count: number;
     unpaid_invoices_count: number;
     amount_collected_today: number;
+    items_amount_collected_today: number;
   } = {
     company_balance: 0,
     customers_count: 0,
     unpaid_invoices_count: 0,
     amount_collected_today: 0,
+    items_amount_collected_today: 0,
   };
   currentUser: User;
   isLoading: boolean = true;
+  UserRoles = UserRoles;
 
   constructor(
     private commonService: CommonService,
     private alertService: AlertService,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private modalService: NgbModal
+  ) {
+    this.currentUser = this.authService.currentUser;
+  }
 
   async ngOnInit(): Promise<void> {
     try {
@@ -39,5 +49,34 @@ export class EmployeesHomeComponent implements OnInit {
       console.log(e);
       this.alertService.toastError('Error fetching dashboard stats');
     }
+  }
+
+  openAddAmountModal() {
+    const modalRef = this.modalService.open(AddReceivedAmountComponent);
+    modalRef.result.then(
+      (result) => {
+        if (result) {
+          if (result) window.location.reload();
+        }
+      },
+      (rejected) => {}
+    );
+  }
+
+  openListAmountsModal() {
+    const modalRef = this.modalService.open(ListReceivedAmountsComponent, {
+      animation: true,
+      centered: true,
+      keyboard: true,
+      size: 'lg',
+    });
+    modalRef.result.then(
+      (result) => {
+        if (result) {
+          if (result) window.location.reload();
+        }
+      },
+      (rejected) => {}
+    );
   }
 }
