25593b8e52d18f6651cb15c03bde8b4aa0af942a
alikashmar8 <alikashmar8@gmail.com>
2024-06-30 21:55:44 +0300

    Add month names in English and Arabic

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,34 @@
 export const primaryColor = '#6f86d6';
 export const loadingGifUrl = 'https://i.gifer.com/VAyR.gif';
 
+export const SCREEN_TIMEOUT_IN_SECONDS: number = 30 * 60; // 30 minutes
 
-export const MANAGER_RENEW_AMOUNT = 20;
-export const SUPERVISOR_RENEW_AMOUNT = 15;
-export const COLLECTOR_RENEW_AMOUNT = 15;
+export const monthNamesEn = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-export const SCREEN_TIMEOUT_IN_SECONDS: number = (30 * 60); // 30 minutes
+export const monthNamesAr = [
+  'كانون الثاني',
+  'شباط',
+  'آذار',
+  'نيسان',
+  'أيار',
+  'حزيران',
+  'تموز',
+  'آب',
+  'أيلول',
+  'تشرين الأول',
+  'تشرين الثاني',
+  'كانون الأول',
+];
