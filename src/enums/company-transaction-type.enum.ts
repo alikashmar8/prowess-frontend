666a58e17b4fe8328d902122a35d4e47dbff5336
alikashmar8 <alikashmar8@gmail.com>
2024-09-05 20:31:41 +0300

    add company transaction type enum

---
+++ src/enums/company-transaction-type.enum.ts
@@ -0,0 +1,10 @@
+export enum CompanyTransactionType {
+  EMPLOYEE_CREATED = 'employee_created',
+  EMPLOYEE_RENEWED = 'employee_renewed',
+  EMPLOYEE_ROLE_CHANGED = 'employee_role_changed',
+  SUB_EMPLOYEE_CREATED = 'sub_employee_created',
+  SUB_EMPLOYEE_RENEWED = 'sub_employee_renewed',
+  SUB_EMPLOYEE_ROLE_CHANGED = 'sub_employee_role_changed',
+  MONEY_TRANSFER = 'money_transfer',
+  COMPANY_CREATE = 'company_create',
+}
