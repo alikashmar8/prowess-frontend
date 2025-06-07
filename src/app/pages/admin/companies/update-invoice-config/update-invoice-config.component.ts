654bf35d7bd7882b3efbb74d0f56eb10214f46aa
alikashmar8 <alikashmar8@gmail.com>
2025-06-07 23:33:48 +0300

    add update invoice configuration component

---
+++ src/app/pages/admin/companies/update-invoice-config/update-invoice-config.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InvoiceConfig } from 'src/models/invoice-config.model';
+import { InvoiceConfigsService } from './../../../../services/invoice-config.service';
+
+@Component({
+  selector: 'app-update-invoice-config',
+  templateUrl: './update-invoice-config.component.html',
+  styleUrls: ['./update-invoice-config.component.css'],
+})
+export class UpdateInvoiceConfigComponent implements OnInit {
+  invoiceConfigId: string;
+  invoiceConfig: InvoiceConfig = {
+    id: null,
+    printInvoiceId: false,
+    printCustomerName: false,
+    printInvoicePlans: false,
+    createdAt: null,
+    updatedAt: null,
+    company: null,
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private invoiceConfigsService: InvoiceConfigsService
+  ) {}
+
+  ngOnInit(): void {
+    this.invoiceConfigId = this.route.snapshot.paramMap.get('id');
+    this.loadInvoiceConfig();
+  }
+
+  async loadInvoiceConfig() {
+    this.invoiceConfig = await this.invoiceConfigsService.getById(
+      this.invoiceConfigId
+    );
+  }
+
+  async updateInvoiceConfig() {
+    (
+      await this.invoiceConfigsService.update(
+        this.invoiceConfigId,
+        this.invoiceConfig
+      )
+    ).subscribe(() => {
+      alert('Invoice configuration updated successfully.');
+    });
+  }
+}
