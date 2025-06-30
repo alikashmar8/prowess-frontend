808a9ce5b2e183f986eb9c46471c5ed53cc889b1
alikashmar8 <alikashmar8@gmail.com>
2025-06-30 23:07:48 +0300

    add price per counter input to edit plan modal for per counter invoice type

--- src/app/common/modals/edit-plan-modal/edit-plan-modal.component.ts
+++ src/app/common/modals/edit-plan-modal/edit-plan-modal.component.ts
@@ -1,9 +1,12 @@
-import { loadingGifUrl } from 'src/constants/constants';
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { PlansService } from 'src/app/services/plans.service';
-import { Plan } from 'src/models/plan.model';
 import { AlertService } from 'src/app/services/alert.service';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { PlansService } from 'src/app/services/plans.service';
+import { loadingGifUrl } from 'src/constants/constants';
+import { CompanyInvoicesType } from 'src/enums/company-invoices-type.enum';
+import { Plan } from 'src/models/plan.model';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-edit-plan-modal',
@@ -14,12 +17,17 @@ export class EditPlanModalComponent implements OnInit {
   @Input() plan: Plan;
   isUpdateLoading: boolean = false;
   loadingGifUrl = loadingGifUrl;
+  currentUser: User;
+  CompanyInvoicesType = CompanyInvoicesType;
 
   constructor(
     public activeModal: NgbActiveModal,
     private plansService: PlansService,
-    private alertService: AlertService
-  ) {}
+    private alertService: AlertService,
+    private authService: AuthService
+  ) {
+    this.currentUser = this.authService.currentUser;
+  }
 
   ngOnInit(): void {}
 
@@ -43,6 +51,7 @@ export class EditPlanModalComponent implements OnInit {
         name: this.plan.name,
         price: Number(this.plan.price),
         isActive: this.plan.isActive,
+        pricePerCounter: this.plan.pricePerCounter,
       })
       .subscribe(
         (result: any) => {
