a33c0e795fffc61bc816a2dbaa243765c311bb57
alikashmar8 <alikashmar8@gmail.com>
2025-07-04 23:20:54 +0300

    refactor invoice fetching logic to use filters and improve Excel export functionality

--- src/app/pages/employees/invoices/items-invoices/items-invoices.component.ts
+++ src/app/pages/employees/invoices/items-invoices/items-invoices.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth-service.service';
 import { InvoicesService } from 'src/app/services/invoices.service';
 import { LoadingService } from 'src/app/services/loading.service';
+import { InvoiceTypes } from 'src/enums/invoices-type.enum';
 import { Invoice } from 'src/models/invoice.model';
 import { User } from 'src/models/user.model';
 
@@ -20,6 +21,7 @@ export class ItemsInvoicesComponent implements OnInit {
   currentUser: User;
   totalPages: number;
   sum = '';
+  filters: Record<string, any> = {};
 
   constructor(
     private invoicesService: InvoicesService,
@@ -30,12 +32,12 @@ export class ItemsInvoicesComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     try {
       this.loadingService.appLoading(true);
-      let res = await this.invoicesService.getItemsInvoices({
+      this.filters = {
+        type: InvoiceTypes.ITEMS_INVOICE,
         take: this.take,
         skip: this.skip,
-      });
-      console.log(res);
-
+      };
+      let res = await this.invoicesService.listInvoices(this.filters);
       this.invoices = res.data;
       this.sum = res.sum;
       this.totalRecords = res.count;
@@ -64,21 +66,25 @@ export class ItemsInvoicesComponent implements OnInit {
   }) {
     try {
       this.isLoading = this.loadingService.appLoading(true);
-      const res = await this.invoicesService.getItemsInvoices({
+      this.filters = {
+        type: InvoiceTypes.ITEMS_INVOICE,
         search: data.search,
         employee_id: data.selectedEmployee,
         plan_id: data.selectedPlan,
         startDate: data.startDateFilter,
         endDate: data.endDateFilter,
-        isPaid: data.isPaid,
+        isPaid: ![undefined, '', null].includes(data.isPaid)
+          ? Boolean(data.isPaid)
+          : null,
         level5Address: data.selectedLevel5Address,
         level4Address: data.selectedLevel4Address,
         level3Address: data.selectedLevel3Address,
         level2Address: data.selectedLevel2Address,
         level1Address: data.selectedLevel1Address,
-        take: data.take,
-        skip: data.skip,
-      });
+        take: data.take || this.take,
+        skip: data.skip || this.skip,
+      };
+      const res = await this.invoicesService.listInvoices(this.filters);
       this.invoices = res.data;
       this.sum = res.sum;
       this.totalRecords = res.count;
@@ -104,41 +110,43 @@ export class ItemsInvoicesComponent implements OnInit {
   async exportExcel() {
     try {
       this.isLoading = this.loadingService.appLoading(true);
-      const ids = this.invoices.map((invoice) => invoice.id);
-      const res = this.invoicesService.downloadInvoicesExcel(ids).subscribe(
-        (res) => {
-          var newBlob = new Blob([res], {
-            type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-          });
-          const data = window.URL.createObjectURL(newBlob);
-          var link = document.createElement('a');
-          link.href = data;
-          link.download =
-            this.currentUser.company.name +
-            '-items-invoices-' +
-            new Date().getDate() +
-            '-' +
-            (new Date().getMonth() + 1) +
-            '-' +
-            new Date().getFullYear() +
-            '.xlsx';
-          link.dispatchEvent(
-            new MouseEvent('click', {
-              bubbles: true,
-              cancelable: true,
-              view: window,
-            })
-          );
-          setTimeout(function () {
-            window.URL.revokeObjectURL(data);
-            link.remove();
-          }, 100);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-      this.isLoading = this.loadingService.appLoading(false);
+      const res = this.invoicesService
+        .downloadInvoicesExcel(this.filters)
+        .subscribe(
+          (res) => {
+            var newBlob = new Blob([res], {
+              type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            });
+            const data = window.URL.createObjectURL(newBlob);
+            var link = document.createElement('a');
+            link.href = data;
+            link.download =
+              this.currentUser.company.name +
+              '-items-invoices-' +
+              new Date().getDate() +
+              '-' +
+              (new Date().getMonth() + 1) +
+              '-' +
+              new Date().getFullYear() +
+              '.xlsx';
+            link.dispatchEvent(
+              new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+              })
+            );
+            setTimeout(function () {
+              window.URL.revokeObjectURL(data);
+              link.remove();
+            }, 100);
+            this.isLoading = this.loadingService.appLoading(false);
+          },
+          (err) => {
+            console.log(err);
+            this.isLoading = this.loadingService.appLoading(false);
+          }
+        );
     } catch (err) {
       this.isLoading = this.loadingService.appLoading(false);
     }
