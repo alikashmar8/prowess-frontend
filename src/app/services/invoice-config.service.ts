d4cbc3c8060ed27a2ceeb34a4ab0a5209e2c73ee
alikashmar8 <alikashmar8@gmail.com>
2025-07-10 23:49:53 +0300

    add InvoiceConfigsService for managing invoice configuration retrieval and updates

---
+++ src/app/services/invoice-config.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InvoiceConfig } from 'src/models/invoice-config.model';
+import { invoiceConfigsEndpoint } from './../../constants/api-constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InvoiceConfigsService {
+  // private apiUrl = environment.apiUrl + '/invoice-configs';
+
+  constructor(private http: HttpClient) {}
+
+  getById(id: string): Promise<InvoiceConfig> {
+    return this.http
+      .get<InvoiceConfig>(`${invoiceConfigsEndpoint}/${id}`)
+      .toPromise();
+  }
+
+  update(
+    id: string,
+    invoiceConfig: InvoiceConfig
+  ): Promise<Observable<InvoiceConfig>> {
+    return Promise.resolve(
+      this.http.patch<InvoiceConfig>(
+        `${invoiceConfigsEndpoint}/${id}`,
+        invoiceConfig
+      )
+    );
+  }
+}
